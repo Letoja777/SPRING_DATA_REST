b839f6670794579fd0ac5b39f16ad7134ef74d00
letoja777 <letoja777@gmail.com>
2022-09-08 13:40:56 +0300

    Initial commit

---
+++ src/main/java/com/ledenov/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.ledenov.spring.springboot.spring_data_rest.dao;
+
+
+import com.ledenov.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository <Employee, Integer> {
+
+}
